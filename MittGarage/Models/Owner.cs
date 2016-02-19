57e81f5045355771a0bf3ac321d553642fca977d
Elev <Elev>
2016-02-19 15:39:53 +0100

    added missing files

---
+++ MittGarage/Models/Owner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MittGarage.Models
+{
+    public class Owner
+    {
+        [Key]
+        public int OwnerID { get; set; }
+
+        public string Name { get; set; }
+        
+        public Owner(string name)
+        {
+            Name = name;
+        }
+        public Owner(){}
+    }
+}
---
+++ MittGarage/Models/VehicleType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MittGarage.Models
+{
+    public class VehicleType
+    {
+        [Key]
+        public int VTID { get; set; }
+
+        public string VType { get; set; }
+
+        public VehicleType(string type)
+        {
+            VType = type;
+        }
+        public VehicleType(){}
+    }
+}
