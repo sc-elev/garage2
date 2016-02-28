3f34a7e8f19454fdd1c31a59540fd8f5555e0c90
John Doe <john.doe@nowhere.net>
2016-02-28 20:50:00 +0100

    Added ng-app skeleton definitions.

---
+++ MittGarage/Scripts/admin.js
@@ -0,0 +1,30 @@
+﻿var GarageApp = angular.module('GarageApp', []);
+
+
+GarageApp.factory('GarageService', ['$http', function ($http) {
+    var GarageService = {};
+
+    GarageService.getVehicles = function () {
+        return $http.get('/Home/GetVehicles');
+    };
+    return GarageService;
+}]);
+
+GarageApp.controller('GarageController', function ($scope, GarageService) {
+
+    $scope.getVehicles = function() {
+        GarageService.getVehicles()
+            .success(function (_vehicles) {
+                $scope.vehicles = _vehicles ? _vehicles : "Nothing";
+                console.log($scope.vehicles);
+            })
+            .error(function (error) {
+                $scope.status = 'Unable to load garage data: ' + error.message;
+                console.log($scope.status);
+            });
+
+    }
+    // $scope.getVehicles()
+
+});
+
