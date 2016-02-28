52ba908a547e292d8f019c9d9dfc0f88931a2fc6
John Doe <john.doe@nowhere.net>
2016-02-28 20:50:00 +0100

    Basic search (not dates) in place.

--- MittGarage/Scripts/admin.js
+++ MittGarage/Scripts/admin.js
@@ -15,10 +15,25 @@ GarageApp.factory('GarageService', ['$http', function ($http) {
 
 GarageApp.controller('GarageController', function ($scope, GarageService) {
     console.log("Starting controller.")
+    function filter(collection, predicate)
+    {
+        var result = new Array();
+        var length = collection.length;
+
+        for(var j = 0; j < length; j++) {
+            if(predicate(collection[j]) == true) {
+                result.push(collection[j]);
+            }
+        }
+        return result;
+    }
+
     $scope.getVehicles = function() {
         GarageService.getVehicles()
             .success(function (_vehicles) {
-                $scope.vehicles = _vehicles ? _vehicles : "Nothing";
+                $scope.allVehicles = _vehicles ? _vehicles : "Nothing";
+                $scope.vehicles =
+                    JSON.parse(JSON.stringify($scope.allVehicles));
                 console.log($scope.vehicles);
             })
             .error(function (error) {
@@ -26,14 +41,26 @@ GarageApp.controller('GarageController', function ($scope, GarageService) {
                     error.message;
                 console.log($scope.status);
             });
-
+    }
+    $scope.onFilterButton = function () {
+        $scope.vehicles = JSON.parse(JSON.stringify($scope.allVehicles));
+        $scope.vehicles = filter($scope.vehicles, function(el) {
+            return !$scope.searchString ||
+                (el.RegNr && el.RegNr.indexOf($scope.searchString) != -1)
+        })
+        $scope.vehicles = filter($scope.vehicles, function (el) {
+            return $scope.selectedType == '-' ||
+                   $scope.selectedType == el.Typename;
+        })
     }
     $scope.showDetails = false
+    $scope.searchString = ""
     $scope.getVehicles()
-    $scope.sëlectedType = "´car"
+    $scope.selectedType = "-"
     GarageService.getTypes()
             .success(function (_types) {
                 $scope.types = _types;
+                $scope.types.unshift("-")
                 console.log($scope.types);
             })
             .error(function (error) {
