d6b7e89397970c1f1a001f2cb5ff80236b29b89d
John Doe <john.doe@nowhere.net>
2016-03-01 13:43:28 +0100

    Adding midding file garage.checkout.js

---
+++ MittGarage/Scripts/garage.checkout.js
@@ -0,0 +1,24 @@
+﻿angular.module('Garage.Checkout',
+               ['Garage.Commons', 'ngRoute'], function ($locationProvider) {
+                   $locationProvider.html5Mode({
+                       enabled: true,
+                       requireBase: false
+                   })
+               })
+.controller('CheckoutController', ['$scope', '$location', 'GarageService',
+    function ($scope, $location, GarageService) {
+        console.debug('Starting controller')
+        function filter(collection, predicate) {
+            // Return collection of items where
+            // predicate(item) is true.
+            var result = new Array();
+            for (var j = 0; j < collection.length; j++) {
+                if (predicate(collection[j]))
+                    result.push(collection[j]);
+            }
+            return result;
+        }
+        var id = $location.path().split('/').pop()
+        GarageService.getVehicle($scope, id)
+    }
+])
